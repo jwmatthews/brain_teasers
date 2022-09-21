813b9309ffb9ebbe38ea6bfa647ccde026a5e428
John Matthews <jwmatthews@gmail.com>
2022-09-21 07:33:42 -0400

    Added try_into example

--- rust/type_conversion/src/main.rs
+++ rust/type_conversion/src/main.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 fn main() {
     let x: u64 = 4_294_967_296;
     let y = x as u32;
@@ -6,4 +8,6 @@ fn main() {
     } else {
         println!("x does not equal y.");
     }
+
+    let _z: u32 = (4_294_967_296_u64).try_into().expect("Conversion error");
 }
